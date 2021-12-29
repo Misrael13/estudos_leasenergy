458bfb6b30b62d64e0374eee66eece4fdcdfa3e8
Misrael13 <93010086+Misrael13@users.noreply.github.com>
2021-12-29 14:08:55 -0300

    conversão do valor kwh, de texto para número (float)

--- scrap2.py
+++ scrap2.py
@@ -12,10 +12,11 @@ def scraping(url, el_src, el_type = By.ID):
     BeautifulSoup(browser.page_source, 'html.parser')
     sleep(5.0)
     tag_enc2 = browser.find_element(el_type, el_src)
-    return tag_enc2.text
+    return tag_enc2.text.replace(',' , '.')[:7]
 
 valor1 = scraping('https://controlemais.com.br/monitoramento/medidor/1128', 'consumo-kwh')
 sleep(1.5)
-valor2 = scraping('https://www.globo.com/', 'homeui-tc-podcasts', By.CLASS_NAME)
-print('\033[33m' 'o valor em kwh é', valor1, '\033[m')
-print('\033[32m', valor2, '\033[m' )
+# valor2 = scraping('https://www.globo.com/', 'homeui-tc-podcasts', By.CLASS_NAME)
+v1 = float(valor1)
+print('\033[33m' 'o valor em kwh é', v1, '\033[m')
+# print('\033[32m', valor2, '\033[m' )
